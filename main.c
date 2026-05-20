fc8b78eaa29ecdc77ad3c49ed9c46eaab464df1d
ota <chinthyarahmaqa@gmail.com>
2026-05-21 00:28:23 +0700

    enum selesai

--- main.c
+++ main.c
@@ -3,22 +3,35 @@
 #include <stdlib.h>
 
 typedef enum {
-    JALAN_KAKI,
-    OJEK,
-    BIKUN
+    JALAN_KAKI = 0,
+    SEPEDA     = 1,
+    MOTOR      = 2,
+    OJEK       = 3,
+    BIKUN      = 4,
+    KRL        = 5
 } ModaTransportasi;
 
 typedef enum {
-    AMAN,
-    CUKUP_AMAN,
-    BERISIKO
+    AMAN        = 2,
+    CUKUP_AMAN  = 1,
+    BERISIKO    = 0
 } TingkatKeamanan;
 
 typedef struct {
-    char nama_rute[120];
-    char tujuan[60];
+    char             nama_rute[120];
+    char             asal[60];
+    char             tujuan[60];
     ModaTransportasi moda;
-    int skor_total;
+    float            jarak_km;
+    int              waktu_menit;
+    int              estimasi_biaya;
+    int              skor_lampu;
+    int              skor_kepadatan;
+    int              skor_gsm;
+    int              skor_kriminal;
+    int              skor_total;
+    TingkatKeamanan  tingkat;
+    char             catatan[100];
 } Rute;
 
 Rute semuaRute[] = {
@@ -54,6 +67,15 @@ void sortRute(){
 int main(){
     printf("1. Kukusan Teknik\n");
 printf("0. Keluar\n");
-
+printf("1. Kukusan Teknik\n");
+printf("0. Keluar\n");
+printf("1. Kukusan Teknik\n");
+printf("0. Keluar\n");
+ printf("1. Kukusan Teknik\n");
+printf("0. Keluar\n");
+printf("1. Kukusan Teknik\n");
+printf("0. Keluar\n");
+printf("1. Kukusan Teknik\n");
+printf("0. Keluar\n");
     return 0;
 }
